88f6ce5a040c974ab4e887a1644f7386fd8bc623
Okafor Patrick C <patrickpeters911@gmail.com>
2024-05-21 15:13:17 +0000

    start api

--- api/v1/app.py
+++ api/v1/app.py
@@ -28,4 +28,3 @@ if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
-
